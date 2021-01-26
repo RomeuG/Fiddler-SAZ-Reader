30d6831794e5d63e9bfa69369ef8c3c7160b5c7b
Romeu Vieira <romeu.bizz@gmail.com>
2021-01-26 23:01:28 +0000

    Define layout

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,7 @@
 use relm_derive::Msg;
+use relm_derive::widget;
 use relm::{connect, Relm, Update, Widget};
+use gtk::Orientation;
 use gtk::prelude::*;
 use gtk::{Window, Inhibit, WindowType};
 
@@ -10,7 +12,7 @@ enum Msg {
 }
 
 struct Model {
-    // …
+    // relm: relm::Relm<Win>
 }
 
 struct Win {
@@ -43,6 +45,7 @@ impl Update for Win {
 }
 
 impl Widget for Win {
+
     // Specify the type of the root widget.
     type Root = Window;
 
@@ -55,6 +58,43 @@ impl Widget for Win {
     fn view(relm: &Relm<Self>, model: Self::Model) -> Self {
         // GTK+ widgets are used normally within a `Widget`.
         let window = Window::new(WindowType::Toplevel);
+        window.set_resizable(false);
+
+        let hbox = gtk::Box::new(Orientation::Horizontal, 0);
+
+		let table = gtk::TextView::new();
+		table.set_property_width_request(300);
+		table.set_margin_start(8);
+		table.set_margin_top(8);
+		table.set_margin_bottom(8);
+		table.get_buffer().unwrap().set_text("Table text!");
+
+        let vbox = gtk::Box::new(Orientation::Vertical, 0);
+		vbox.set_margin_start(8);
+		vbox.set_margin_end(8);
+		vbox.set_margin_top(8);
+		vbox.set_margin_bottom(8);
+
+		let requestTextView = gtk::TextView::new();
+		requestTextView.set_property_height_request(300);
+		requestTextView.set_property_width_request(300);
+		requestTextView.set_editable(false);
+		requestTextView.get_buffer().unwrap().set_text("Request text!");
+
+		let responseTextView = gtk::TextView::new();
+		responseTextView.set_property_height_request(300);
+		responseTextView.set_property_width_request(300);
+		responseTextView.set_editable(false);
+		responseTextView.set_margin_top(8);
+		responseTextView.get_buffer().unwrap().set_text("Response text!");
+
+		vbox.add(&requestTextView);
+		vbox.add(&responseTextView);
+
+		hbox.add(&table);
+		hbox.add(&vbox);
+
+		window.add(&hbox);
 
         // Connect the signal `delete_event` to send the `Quit` message.
         connect!(relm, window, connect_delete_event(_, _), return (Some(Msg::Quit), Inhibit(false)));
@@ -65,11 +105,16 @@ impl Widget for Win {
 
         Win {
             model,
-            window: window,
+            window,
         }
     }
 }
 
 fn main() {
+    // let args: Vec<String> = env::args().collect();
+	// let sazfile = &args[1];
+
+	// do saz parsing here before going to the GUI
+
     Win::run(()).unwrap();
 }
