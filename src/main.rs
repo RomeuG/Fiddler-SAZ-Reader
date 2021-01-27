43cf5339774c429c77b2c4444800cf3708e57f83
Romeu Vieira <romeu.bizz@gmail.com>
2021-01-27 23:22:13 +0000

    Tree view columns

--- src/main.rs
+++ src/main.rs
@@ -44,6 +44,17 @@ impl Update for Win {
     }
 }
 
+fn append_column(tree: &gtk::TreeView, title: &str, id: i32) {
+    let column = gtk::TreeViewColumn::new();
+    let cell = gtk::CellRendererText::new();
+
+    column.pack_start(&cell, true);
+    // Association of the view's column with the model's `id` column.
+    column.add_attribute(&cell, "text", id);
+    column.set_title(&title);
+    tree.append_column(&column);
+}
+
 impl Widget for Win {
 
     // Specify the type of the root widget.
@@ -62,39 +73,57 @@ impl Widget for Win {
 
         let hbox = gtk::Box::new(Orientation::Horizontal, 0);
 
-		let table = gtk::TextView::new();
-		table.set_property_width_request(300);
-		table.set_margin_start(8);
-		table.set_margin_top(8);
-		table.set_margin_bottom(8);
-		table.get_buffer().unwrap().set_text("Table text!");
+	// let table = gtk::TextView::new();
+	// table.set_property_width_request(300);
+	// table.set_margin_start(8);
+	// table.set_margin_top(8);
+	// table.set_margin_bottom(8);
+	// table.get_buffer().unwrap().set_text("Table text!");
+
+	let tree_view_model = gtk::ListStore::new(&[String::static_type(), String::static_type()]);
+
+	let test_entries1 = &["Test1", "Test2", "Test3"];
+	let test_entries2 = &["Test1", "Test2", "Test3"];
+
+	for i in 0..3 {
+	    tree_view_model.insert_with_values(None, &[0, 1], &[&test_entries1[i], &test_entries2[i]]);
+	}
+
+	let tree = gtk::TreeView::new();
+	tree.set_property_width_request(300);
+	tree.set_headers_visible(true);
+	// Creating the two columns inside the view.
+	append_column(&tree, "Title1", 0);
+	append_column(&tree, "Title2", 1);
+
+	tree.set_model(Some(&tree_view_model));
 
         let vbox = gtk::Box::new(Orientation::Vertical, 0);
-		vbox.set_margin_start(8);
-		vbox.set_margin_end(8);
-		vbox.set_margin_top(8);
-		vbox.set_margin_bottom(8);
+	vbox.set_margin_start(8);
+	vbox.set_margin_end(8);
+	vbox.set_margin_top(8);
+	vbox.set_margin_bottom(8);
 
-		let requestTextView = gtk::TextView::new();
-		requestTextView.set_property_height_request(300);
-		requestTextView.set_property_width_request(300);
-		requestTextView.set_editable(false);
-		requestTextView.get_buffer().unwrap().set_text("Request text!");
+	let requestTextView = gtk::TextView::new();
+	requestTextView.set_property_height_request(300);
+	requestTextView.set_property_width_request(300);
+	requestTextView.set_editable(false);
+	requestTextView.get_buffer().unwrap().set_text("Request text!");
 
-		let responseTextView = gtk::TextView::new();
-		responseTextView.set_property_height_request(300);
-		responseTextView.set_property_width_request(300);
-		responseTextView.set_editable(false);
-		responseTextView.set_margin_top(8);
-		responseTextView.get_buffer().unwrap().set_text("Response text!");
+	let responseTextView = gtk::TextView::new();
+	responseTextView.set_property_height_request(300);
+	responseTextView.set_property_width_request(300);
+	responseTextView.set_editable(false);
+	responseTextView.set_margin_top(8);
+	responseTextView.get_buffer().unwrap().set_text("Response text!");
 
-		vbox.add(&requestTextView);
-		vbox.add(&responseTextView);
+	vbox.add(&requestTextView);
+	vbox.add(&responseTextView);
 
-		hbox.add(&table);
-		hbox.add(&vbox);
+	hbox.add(&tree);
+	hbox.add(&vbox);
 
-		window.add(&hbox);
+	window.add(&hbox);
 
         // Connect the signal `delete_event` to send the `Quit` message.
         connect!(relm, window, connect_delete_event(_, _), return (Some(Msg::Quit), Inhibit(false)));
@@ -112,9 +141,9 @@ impl Widget for Win {
 
 fn main() {
     // let args: Vec<String> = env::args().collect();
-	// let sazfile = &args[1];
+    // let sazfile = &args[1];
 
-	// do saz parsing here before going to the GUI
+    // do saz parsing here before going to the GUI
 
     Win::run(()).unwrap();
 }
